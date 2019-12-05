3a5dd7405c001baaee278e54537d86db3b9b676f
Syoya Tsuha <e185735@ie.u-ryukyu.ac.jp>
2019-12-05 17:52:56 +0900

    Initial Commit

---
+++ GroupF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupF
+//
+//  Created by 津覇　翔也 on 2019/12/05.
+//  Copyright © 2019 津覇　翔也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
